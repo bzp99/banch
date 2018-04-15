78bd5630ae873e654fc0f7c84bf8454506e34d55
bertalanp99 <peter.bertalan.root@gmail.com>
2018-04-15 17:39:21 +0200

    doing memory allocs here is overkill

--- test/test_nostl/nostl_set_test.cxx
+++ test/test_nostl/nostl_set_test.cxx
@@ -4,39 +4,35 @@
 TEST(SetTest, CreateEmpty)
 {
 	// create empty set and check if its size is 0
-	nostl::Set<int> * foo = new nostl::Set<int>;
-	EXPECT_EQ(0, foo->size());
-	delete foo;
+	nostl::Set<int> foo;
+	EXPECT_EQ(0, foo.size());
 }
 
 TEST(SetTest, Fill)
 {
-	// fill new set with different doubles, namely {0, .5, 1, ..., 50}
-	nostl::Set<double> * foo = new nostl::Set<double>;
+	// create new set with different doubles, namely {0, .5, 1, ..., 50}
+	nostl::Set<double> foo;
 	for (unsigned int i = 0; i < 100; ++i)
 	{
-		foo->insert(static_cast<double>(i) / 2.0);
+		foo.insert(static_cast<double>(i) / 2.0);
 	}
-	EXPECT_EQ(100, foo->size());
+	EXPECT_EQ(100, foo.size());
 
 	// now try to add already existing elements
-	foo->insert(1.0);
-	foo->insert(0);
-	EXPECT_EQ(100, foo->size());
-
-	delete foo;
+	foo.insert(1.0);
+	foo.insert(0);
+	EXPECT_EQ(100, foo.size());
 }
 
 TEST(SetTest, Clear)
 {
 	// fill new set with longs and test clearing
-	nostl::Set<long> * foo = new nostl::Set<long>;
+	nostl::Set<long> foo;
 	for (unsigned int i = 0; i < 420; ++i)
 	{
-		foo->insert(static_cast<long>(i) / -2.718281828459);
+		foo.insert(static_cast<long>(i) / -2.718281828459);
 	}
-	EXPECT_EQ(420, foo->size());
-	foo->clear();
-	EXPECT_EQ(0, foo->size());
-	delete foo;
+	EXPECT_EQ(420, foo.size());
+	foo.clear();
+	EXPECT_EQ(0, foo.size());
 }
