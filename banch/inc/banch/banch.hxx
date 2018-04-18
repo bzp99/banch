f2b5e6ddb805652c42f02c1b56a5dd68e102f6c7
bertalanp99 <peter.bertalan.root@gmail.com>
2018-04-18 17:11:10 +0200

    heterogenous container needs to contain pointers and not actual objects

--- banch/inc/banch/banch.hxx
+++ banch/inc/banch/banch.hxx
@@ -10,17 +10,27 @@ class Ingredient {
 public:
 	virtual void print() const;
 
-	virtual bool operator==(Ingredient const &);
+	//virtual bool operator==(Ingredient const &);
 
 	virtual ~Ingredient(); // virtual destructor for this class shall be derived later
 };
 
 class Recipe {
 public:
-	Recipe() : number_of_ingredients_(0) { ingredients_ = new nostl::Set<Ingredient>; }
+	Recipe() : number_of_ingredients_(0)
+	{
+		ingredients_ = new nostl::Set<Ingredient*>;
+	}
 
-	void add(Ingredient const & addendum) { this->ingredients_->insert(addendum); }
-	void remove(Ingredient const & delendum) { this->ingredients_->remove(delendum); }
+	void add(Ingredient const & addendum)
+	{
+		this->ingredients_->insert(addendum);
+	}
+
+	void remove(Ingredient const & delendum)
+	{
+		this->ingredients_->remove(delendum);
+	}
 
 	void show() const;
 
@@ -30,7 +40,7 @@ public:
 	~Recipe() { delete this->ingredients_; }
 
 private:
-	nostl::Set<Ingredient> * ingredients_; // heterogenous container
+	nostl::Set<Ingredient*> * ingredients_; // heterogenous container
 	unsigned int number_of_ingredients_;
 }; // class Recipe
 
