5a9c7f7afdb0de7c52f676aa3c9da56455a139a6
bertalanp99 <peter.bertalan.root@gmail.com>
2018-05-07 00:33:19 +0200

    gtest module for the banch namespace

---
+++ test/test_banch/banch_recipe_test.cxx
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "banch/banch.hxx"
+
+using namespace banch;
+
+TEST(RecipeTest, CreateRecipe)
+{
+	// create recipe and fill with stuff
+	Recipe dryMartini("Dry Martini");
+	dryMartini.add(new Beverage("gin", 5));
+	dryMartini.add(new Beverage("dry vermouth", 1));
+	dryMartini.add(new Extra("a few olives"));
+
+	// test printing recipe
+	std::stringstream ss;
+	dryMartini.show(ss);
+	EXPECT_STREQ("Recipe: Dry Martini\n \
+					5 units of gin\n \
+					1 units of dry vermouth\n \
+					a few olives\n",
+					ss.str().c_str());
+}
