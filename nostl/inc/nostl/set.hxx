1843e82439835c1f43835ec972f9a1ee78e5e531
bertalanp99 <peter.bertalan.root@gmail.com>
2018-05-07 00:31:25 +0200

    list class's iterator can be used in a more straightforward way

--- nostl/inc/nostl/set.hxx
+++ nostl/inc/nostl/set.hxx
@@ -7,6 +7,7 @@
 
 #include "nostl/list.hxx"
 
+/// \brief namespace for STL reimplementations
 namespace nostl {
 
 /// \brief re-implementation of std::Set<T>
@@ -15,7 +16,7 @@ namespace nostl {
 ///
 /// I had to re-implement the Set class because using STL containers was
 /// prohibited. This set uses the previously made custom List class. The
-/// only addition is that it checks for multiple addition (the Set can only
+/// only addition is that it checks for multiple addition (a Set may only
 /// contain each element only once).
 template <typename T>
 class Set {
@@ -39,126 +40,31 @@ public:
 	/// \return the Set's size
 	unsigned int size() const { return this->list_.size(); }
 
-
 public:
-	/// \brief custom made iterator to navigate the Set more easily
-	class Iterator {
-	public:
-		/// \brief constructor for the Set's Iterator (uses Lists class')
-		///
-		/// \param list_iterator List Iterator to use
-		Iterator(typename List<T>::Iterator list_iterator)
-		{
-			this->list_iterator_ = list_iterator;
-		}
+	/// \brief use the List class's Iterator
+	using Iterator = typename nostl::List<T>::Iterator;
 
-
-		/// \brief dereference operator
-		///
-		/// \return the value of the List Node currently pointed to
-		T & operator*() { return *(this->list_iterator_); }
-
-		/// \brief const dereference operator
-		///
-		/// \return the value of the List Node currently pointed to
-		T const & operator*() const { return *(this->list_iterator_); }
-
-		/// \brief member access operator
-		///
-		/// \return pointer to itself
-		Iterator * operator->() const { return this; }
-
-
-		/// \brief preincrement operator
-		///
-		/// \return Iterator of next List Node
-		Iterator operator++()
-		{
-			++this->list_iterator_;
-			return *this;
-		}
-
-		/// \brief postincrement operator
-		///
-		/// \param int dummy parameter
-		///
-		/// \return Iterator of current List Node
-		Iterator operator++(int)
-		{
-			Iterator rv = *this;
-			++this->list_iterator_;
-			return rv;
-		}
-
-		/// \brief predecrement operator
-		///
-		/// \return Iterator of previous List Node
-		Iterator operator--()
-		{
-			--this->list_iterator_;
-			return *this;
-		}
-
-		/// \brief postdecrement operator
-		///
-		/// \param int dummy parameter
-		///
-		/// \return Iterator of current List Node
-		Iterator operator--(int)
-		{
-			Iterator rv = *this;
-			--this->list_iterator_;
-			return rv;
-		}
-
-
-		/// \brief equality operator
-		///
-		/// \param rhs Iterator to check equality with
-		///
-		/// \return true if the Iterators point to the same List Node
-		bool operator==(Iterator const & rhs) const
-		{
-			return this->list_iterator_ == rhs.list_iterator_;
-		}
-
-		/// \brief inequality operator
-		///
-		/// \param rhs Iterator to check inequality with
-		///
-		/// \return true if the Iterators point to different List Nodes
-		bool operator!=(Iterator const & rhs) const { return !(*this == rhs); }
-
-
-	private:
-		typename List<T>::Iterator list_iterator_; ///< using already
-													///< implemented List
-													/// Itearator
-	}; // class Iterator
-
-public:
 	/// \brief get Iterator to the first element of the Set
 	///
 	/// \return Iterator to first element
-	Iterator begin()
+	Iterator begin() const
 	{
 		typename List<T>::Iterator i = this->list_.begin();
-		return Iterator(i);
+		return i;
 	}
 
 	/// \brief get Iterator to the last element of the Set
 	///
 	/// \return Iterator to last element
-	Iterator end()
+	Iterator end() const
 	{
 		typename List<T>::Iterator i = this->list_.end();
-		return Iterator(i);
+		return i;
 	}
 
 
 private:
 	List<T> list_; ///< Set is implemented using a doubly-linked List
-
 }; // class Set
 
 template <typename T>
@@ -192,6 +98,6 @@ void Set<T>::clear()
 	this->list_.clear();
 }
 
-}
+} // namespace nostl
 
 #endif // BANCH_NOSTL_SET_HXX
