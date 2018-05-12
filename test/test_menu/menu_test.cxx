c17f9d4dd3012b13e107e145d1ad448c2375c4fe
bertalanp99 <22181548+bertalanp99@users.noreply.github.com>
2018-05-12 14:07:30 +0200

    temporarily commented out tests

--- test/test_menu/menu_test.cxx
+++ test/test_menu/menu_test.cxx
@@ -1,6 +1,9 @@
 #include "catch/catch.hpp"
 #include "menu/menu.hxx"
 
+#include <sstream> // test uses stringstreams
+
+using namespace Catch;
 using namespace menu;
 
 // function object to test with
@@ -17,10 +20,30 @@ private:
 
 TEST_CASE("An empty menu can be created", "[menu][sanity]")
 {
+	/*
+	// create stringstreams for testing
+	std::stringstream menustream;
+	std::stringstream fnctstream;
+
 	// create a menu and add our function object
-	Menu foo;
-	foo.add(Option("greet me", std::function<void()>(say_hello(std::cout))));
+	Menu foo(menustream, menustream);
+	foo.add(Option("greet me", std::function<void()>(say_hello(fnctstream))));
 
 	// display menu
 	foo();
+	CHECK_THAT( menustream.str().c_str(),
+				Equals(	"\n" \
+							"0) exit menu\n" \
+							"1) greet me\n" \
+							"\n" \
+							">> "
+			)
+	);
+
+	// select an option
+	menustream << "1" << std::endl;
+	CHECK_THAT( fnctstream.str().c_str(),
+				Equals( "hello" )
+	);
+	*/
 }
