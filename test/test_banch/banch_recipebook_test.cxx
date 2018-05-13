88bf271dc2b83c4cccf9cc1adddc17f17172bc51
bertalanp99 <22181548+bertalanp99@users.noreply.github.com>
2018-05-13 10:21:22 +0200

    pointers HAVE to be used

--- test/test_banch/banch_recipebook_test.cxx
+++ test/test_banch/banch_recipebook_test.cxx
@@ -10,18 +10,17 @@ TEST_CASE("A recipe book can be created", "[recipebook]")
 {
 	// create some placeholder recipes and put them into the book
 	Recipe * foo = new Recipe("foo");
+	foo->add(new Beverage("mineral water", 10));
+	foo->add(new Extra("lemon slices"));
+	
 	Recipe * bar = new Recipe("bar");
+	bar->add(new Beverage("milk", 8));
 
 	RecipeBook qux;
 	qux.add(foo);
 	qux.add(bar);
 
 	CHECK( qux.number_of_entries() == 2 );
-
-	// add a few ingredients and test listing
-	foo->add(new Beverage("mineral water", 10));
-	foo->add(new Extra("lemon slices"));
-	bar->add(new Beverage("milk", 8));
 
 	std::stringstream ss;
 	qux.list(ss);
@@ -42,7 +41,7 @@ TEST_CASE("Recipes can be removed from a recipe book", "[recipebook]")
 	// create some placeholder recipes with stuff and add in a book
 	Recipe * foo = new Recipe("foo");
 	Recipe * oldCrippledMan = new Recipe("I'm so dead");
-	Recipe * bar = new Recipe("bar");
+	Recipe * bar =  new Recipe("bar");
 
 	RecipeBook qux;
 	qux.add(foo);
