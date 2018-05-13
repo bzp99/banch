aa629ab57cca9437bc02715e52a05e3854c1e97d
bertalanp99 <22181548+bertalanp99@users.noreply.github.com>
2018-05-13 10:19:11 +0200

    started working on main menu

--- banch/src/banch_main.cxx
+++ banch/src/banch_main.cxx
@@ -1,4 +1,37 @@
-int main()
+#include "banch/banch.hxx"
+#include "menu/menu.hxx"
+#include "banch/interactiveFunctions.hxx"
+
+#include <functional>
+
+int main(int argc, char ** argv)
 {
+	banch::RecipeBook myBook;
+	banch::Recipe * mySoleRecipe = new banch::Recipe("tester");
+	mySoleRecipe->add(new banch::Beverage("apple juice", 5));
+	mySoleRecipe->add(new banch::Extra("some extra stuff"));
+	myBook.add(mySoleRecipe);
+
+	menu::Menu mainMenu(std::cout, std::cin);
+	mainMenu.add(menu::Option("list recipes",
+								std::function<void()>(banch::Flist_recipes(
+																	std::cout,
+																	myBook
+																			)
+														)
+
+								)
+	);
+	mainMenu.add(menu::Option("add recipe",
+								std::function<void()>(banch::Fadd_recipe(
+																	std::cout,
+																	std::cin,
+																	myBook
+																		)
+														)
+								)
+	);
+
+	mainMenu();
 	return 0;
 }
